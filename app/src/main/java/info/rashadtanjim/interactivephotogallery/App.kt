c0c9291b74f9bfb404054f95e7960f4353f21aca
MD. Rashad Tanjim <rashad.tanjim@northsouth.edu>
2021-11-22 00:46:31 +0600

    fix: application class modified and context add

--- app/src/main/java/info/rashadtanjim/interactivephotogallery/App.kt
+++ app/src/main/java/info/rashadtanjim/interactivephotogallery/App.kt
@@ -1,7 +1,16 @@
 package info.rashadtanjim.interactivephotogallery
 
 import android.app.Application
+import android.content.Context
 
 class App : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+    }
+
+    companion object {
+        lateinit var appContext: Context
+    }
 }
