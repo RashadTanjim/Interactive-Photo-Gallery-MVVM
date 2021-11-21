af60711e92516e66d2fc2cf7858cd84fbe0824f9
MD. Rashad Tanjim <rashad.tanjim@northsouth.edu>
2021-11-22 00:53:36 +0600

    feat: dark-light mode implementation using preference

--- app/src/main/java/info/rashadtanjim/interactivephotogallery/data/UserPreferences.kt
+++ app/src/main/java/info/rashadtanjim/interactivephotogallery/data/UserPreferences.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
@@ -12,5 +14,18 @@ class UserPreferences @Inject constructor(
 ) {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "photo_store")
 
+    val nightMode: Flow<Boolean?>
+        get() = context.dataStore.data.map { preferences ->
+            preferences[NIGHT_MODE]
+        }
 
+    suspend fun toggleNightMode(isNightMode: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[NIGHT_MODE] = isNightMode
+        }
+    }
+
+    companion object {
+        private val NIGHT_MODE = booleanPreferencesKey("night_mode")
+    }
 }
