309efa4334131544d201a0f32f6bbc68292f0a91
MD. Rashad Tanjim <rashad.tanjim@northsouth.edu>
2021-11-23 06:05:34 +0600

    feat!: adapter class add and gallery data load working

--- app/src/main/java/info/rashadtanjim/interactivephotogallery/ui/fragment/AboutAppFragment.kt
+++ app/src/main/java/info/rashadtanjim/interactivephotogallery/ui/fragment/AboutAppFragment.kt
@@ -1,7 +1,38 @@
 package info.rashadtanjim.interactivephotogallery.ui.fragment
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import info.rashadtanjim.interactivephotogallery.App
+import info.rashadtanjim.interactivephotogallery.data.repository.UserRepository
+import info.rashadtanjim.interactivephotogallery.data.source.remote.PicsumApi
+import info.rashadtanjim.interactivephotogallery.databinding.FragmentAboutAppBinding
+import info.rashadtanjim.interactivephotogallery.ui.SharedViewModel
+import info.rashadtanjim.interactivephotogallery.ui.base.BaseFragment
 
-class AboutAppFragment : Fragment() {
+class AboutAppFragment :
+    BaseFragment<SharedViewModel, FragmentAboutAppBinding, UserRepository>() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun getViewModel() = SharedViewModel::class.java
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentAboutAppBinding.inflate(inflater, container, false)
+
+    override fun getFragmentRepository(): UserRepository {
+        val api = remoteDataSource.buildApi(PicsumApi::class.java)
+        val picsumDao = (requireActivity().applicationContext as App).database.PicsumDao()
+        return UserRepository(api, picsumDao)
+    }
 }
--- app/src/main/java/info/rashadtanjim/interactivephotogallery/ui/fragment/PhotoViewFragment.kt
+++ app/src/main/java/info/rashadtanjim/interactivephotogallery/ui/fragment/PhotoViewFragment.kt
@@ -1,7 +1,65 @@
 package info.rashadtanjim.interactivephotogallery.ui.fragment
 
-import androidx.fragment.app.Fragment
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import coil.load
+import info.rashadtanjim.core.utlis.showToast
+import info.rashadtanjim.interactivephotogallery.R
+import info.rashadtanjim.interactivephotogallery.databinding.FragmentPhotoViewBinding
 
-class PhotoViewFragment : Fragment() {
+class PhotoViewFragment : DialogFragment() {
+    private lateinit var binding: FragmentPhotoViewBinding
 
+    private var selectedPhoto: String? = null
+
+    private val args: PhotoViewFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Material_Light_DialogWhenLarge_NoActionBar)
+        dialog?.window?.navigationBarColor =
+            ResourcesCompat.getColor(resources, R.color.black, null)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPhotoViewBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        args.selectedPhoto?.let {
+            selectedPhoto = it
+        }
+
+        binding.zoomViewPhoto.load(selectedPhoto).also {
+            showToast(getString(R.string.zoom_in_out))
+        }
+
+        binding.imageButtonSave.setOnClickListener {
+
+        }
+
+        binding.imageButtonShare.setOnClickListener {
+
+        }
+
+        binding.imageButtonBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
