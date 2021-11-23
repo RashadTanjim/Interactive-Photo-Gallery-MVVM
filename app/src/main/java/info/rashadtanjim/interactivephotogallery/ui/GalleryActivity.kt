f677b8a99d3bdaf064e3fbdcbafae97f0479d3ac
Md Rashad Tanjim <arnob.tanjim@gmail.com>
2021-11-24 02:04:18 +0600

    BREAKNG CHANGE: Splash Screen add to main activity

---
+++ app/src/main/java/info/rashadtanjim/interactivephotogallery/ui/GalleryActivity.kt
@@ -0,0 +1,27 @@
+package info.rashadtanjim.interactivephotogallery.ui
+
+import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import dagger.hilt.android.AndroidEntryPoint
+import info.rashadtanjim.interactivephotogallery.R
+import info.rashadtanjim.interactivephotogallery.data.UserPreferences
+import info.rashadtanjim.interactivephotogallery.databinding.ActivityGalleryBinding
+import info.rashadtanjim.interactivephotogallery.ui.base.BaseActivity
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class GalleryActivity : BaseActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+
+    @Inject
+    lateinit var userPreferences: UserPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.navigationBarColor =
+            ResourcesCompat.getColor(resources, R.color.background, null)
+    }
+}
